c3550bbaa22d04fa380b2e0ab81153359520faf7
Lenya <leonidivanov826@gmail.com>
2026-03-26 14:00:01 +0200

    added logic and main state properties

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -1,10 +1,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    var gameElements = ["rock", "scissors", "paper"]
+    @State var chooseOfApp = ""
+    @State var chooseOfUser = ""
+    
+    //alerts
+    @State var win = false
+    @State var lose = false
+    @State var draw = false
+    
+    //points' and games' counter
+    @State var counter: Int = 0
+    @State var games: Int = 0
+    
+    
     
     var body: some View {
         NavigationStack {
-            
             ZStack {
                 VStack {
                     VStack {
@@ -21,16 +34,51 @@ struct ContentView: View {
                     VStack {
                         HStack {
                             //buttoms with choicing from user
-
+                            
                         }
                     }
                 }
                 //notification of loss or win on all screen
             }
-
+            
         }
     }
+    
+    func chooseRandomElement() {
+        chooseOfApp = gameElements.randomElement() ?? "rock"
+    }
+    func incrementCounter() {
+        counter += 1
+    }
+    func winningLogic() {
+        if chooseOfUser == chooseOfApp {
+            draw = true
+        } else if chooseOfUser == "rock" && chooseOfApp == "scissors" {
+            win = true
+        } else if chooseOfUser == "scissors" && chooseOfApp == "paper" {
+            win = true
+        } else if chooseOfUser == "paper" && chooseOfApp == "rock" {
+            win = true
+        } else {
+            lose = true
+        }
+    }
+    func losingLogic() {
+        if chooseOfUser == chooseOfApp {
+            draw = true
+        } else if chooseOfApp == "rock" && chooseOfUser == "scissors" {
+            win = true
+        } else if chooseOfApp == "scissors" && chooseOfUser == "paper" {
+            win = true
+        } else if chooseOfApp == "paper" && chooseOfUser == "rock" {
+            win = true
+        } else {
+            lose = true
+        }
+    }
+    
 }
+
 #Preview {
     ContentView()
 }
