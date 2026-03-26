8bbb54c3cb10f97a0935491a2e43fcc6642a61fc
Lenya <leonidivanov826@gmail.com>
2026-03-26 21:52:16 +0200

    refactored some properties, added a second alert, completed the UI (light version), added a score counter and game ending

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -2,120 +2,138 @@ import SwiftUI
 
 struct ContentView: View {
     var gameElements = ["rock", "scissors", "paper"]
-    @State private var chooseOfApp = ""
-    @State private var chooseOfUser = ""
+    @State private var choiceOfApp = ""
+    @State private var choiceOfUser = ""
     @State private var stateOfGameGoal = Bool.random()
     
     //alert
     @State private var scoreTitle = ""
     @State private var showingScore = false
+    @State private var isGameOver = false
     
     //points' and games' counter
     @State private var counter: Int = 0
     @State private var games: Int = 0
     
     
-    
     var body: some View {
         NavigationStack {
             ZStack {
+                Color(.brown)
+                    .opacity(0.2)
+                    .edgesIgnoringSafeArea(.all)
+                
                 VStack {
                     VStack {
                         Text(gameGoal())
+                            .font(Font.headline.bold())
+                        Text(String(counter))
+                            .font(Font.headline.bold())
                     }
                     
                     VStack {
                         Spacer()
-                        Text(chooseOfApp)
-                        Text(chooseOfUser)
+                        Text("The computer chose:")
+                        Text(choiceOfApp)
+                            .font(Font.largeTitle.bold())
                         Spacer()
                     }
                     .onAppear {
                         chooseRandomElement()
                     }
-                    
-                    VStack {
-                        HStack {
-                            buttonsStyle
-                            
-                        }
+                    HStack {
+                        buttonsStyle
                     }
+                    
                 }
             }
+            
             .alert(scoreTitle, isPresented: $showingScore) {
                 Button("Continue") {
+                    if games == 10 {
+                        isGameOver.toggle()
+                        return
+                        
+                    }
                     chooseRandomElement()
-                    stateOfGameGoal.toggle()
-                    chooseOfUser = ""
+                    stateOfGameGoal = Bool.random()
+                    choiceOfUser = ""
                 }
+            }
+            .alert("Game Over", isPresented: $isGameOver) {
+                Button("Start a new game") {
+                    chooseRandomElement()
+                    stateOfGameGoal = Bool.random()
+                    choiceOfUser = ""
+                    counter = 0
+                    games = 0
+                }
+            } message: {
+                Text("Your score is \(counter) points")
             }
         }
     }
     
     
     func gameGoal() -> String {
-        stateOfGameGoal ? "You can Win" : "You can Lose"
+        stateOfGameGoal ? "You should Win" : "You should Lose"
     }
     func chooseRandomElement() {
-        chooseOfApp = gameElements.randomElement() ?? "rock"
+        choiceOfApp = gameElements.randomElement() ?? "Error"
     }
     func incrementCounter() {
         counter += 1
     }
     func winningLogic() {
-        if chooseOfUser == chooseOfApp {
+        if choiceOfUser == choiceOfApp {
             scoreTitle = "It's a draw!"
-            showingScore = true
-        } else if chooseOfUser == "rock" && chooseOfApp == "scissors" {
+        } else if (choiceOfUser == "rock" && choiceOfApp == "scissors") ||
+                    (choiceOfUser == "scissors" && choiceOfApp == "paper") ||
+                    (choiceOfUser == "paper" && choiceOfApp == "rock") {
+            
             scoreTitle = "You won!"
-            showingScore = true
-        } else if chooseOfUser == "scissors" && chooseOfApp == "paper" {
-            scoreTitle = "You won!"
-            showingScore = true
-        } else if chooseOfUser == "paper" && chooseOfApp == "rock" {
-            scoreTitle = "You won!"
-            showingScore = true
+            counter += 1
         } else {
             scoreTitle = "You lost!"
-            showingScore = true
+            if counter > 0 { counter -= 1 }
         }
+        games += 1
+        showingScore = true
     }
     func losingLogic() {
-        if chooseOfUser == chooseOfApp {
+        if choiceOfUser == choiceOfApp {
             scoreTitle = "It's a draw!"
-            showingScore = true
-        } else if chooseOfApp == "rock" && chooseOfUser == "scissors" {
+        } else if (choiceOfApp == "rock" && choiceOfUser == "scissors") ||
+                    (choiceOfApp == "scissors" && choiceOfUser == "paper") ||
+                    (choiceOfApp == "paper" && choiceOfUser == "rock") {
+            
             scoreTitle = "You won!"
-            showingScore = true
-        } else if chooseOfApp == "scissors" && chooseOfUser == "paper" {
-            scoreTitle = "You won!"
-            showingScore = true
-        } else if chooseOfApp == "paper" && chooseOfUser == "rock" {
-            scoreTitle = "You won!"
-            showingScore = true
+            counter += 1
         } else {
             scoreTitle = "You lost!"
-            showingScore = true
+            if counter > 0 { counter -= 1 }
         }
+        games += 1
+        showingScore = true
     }
     
     var buttonsStyle: some View {
         ForEach(gameElements, id: \.self) { element in
             Button {
-                chooseOfUser = element
+                choiceOfUser = element
                 stateOfGameGoal ? winningLogic() : losingLogic()
             } label: {
                 Text(element)
-                    .padding(30)
+                    .frame(width: 100, height: 100)
                     .background(.black.gradient)
-                    .opacity(0.8)
+                    .opacity(0.6)
                     .clipShape(.circle)
+                    .padding(10)
                     .bold()
                     .foregroundColor(.white)
             }
         }
     }
-    
 }
 
 #Preview {
